5bc1f0dd3043fb388572e8e301355a6633deba15
Max Cahill <1bardesign@gmail.com>
2022-01-11 13:51:53 +1100

    ultra basic tile system

---
+++ assets/maps/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tiles" tilewidth="8" tileheight="8" tilecount="1024" columns="32" backgroundcolor="#bed6fd">
+ <image source="../images/tiles.png" width="256" height="256"/>
+</tileset>
---
+++ src/systems/tile_system.lua
@@ -0,0 +1,52 @@
+--ultra-basic tilemap which just renders the first layer of a lua-exported tiled map
+--with a given image and tile size (ie doesn't read out much meta data, just the tile data + dimensions)
+
+local tile_system = class()
+
+function tile_system:new(map, image, tilesize)
+	local layer = map.layers[1]
+	self.size = vec2(layer.width, layer.height)
+	self.tilesize = tilesize
+	self.image = image
+	self.image_layout = vec2(self.image:getDimensions())
+		:vector_div_inplace(tilesize)
+		:floor_inplace()
+	self.quad = lg.newQuad(
+		0, 0,
+		tilesize.x, tilesize.y,
+		self.image:getDimensions()
+	)
+	self.tiles = layer.data
+	self.pos = vec2()
+end
+
+function tile_system:update(dt)
+
+end
+
+function tile_system:draw()
+	--opt: cull drawing to a passed in camera
+	for y = 1, self.size.y do
+		for x = 1, self.size.x do
+			local tx = (x - 1) * self.tilesize.x
+			local ty = (y - 1) * self.tilesize.y
+			local idx = ((x - 1) + (y - 1) * self.size.x) + 1
+			local t = self.tiles[idx] - 1
+			if t >= 0 then
+				self.quad:setViewport(
+					math.floor(t % self.image_layout.x) * self.tilesize.x,
+					math.floor(t / self.image_layout.x) * self.tilesize.y,
+					self.tilesize.x,
+					self.tilesize.y
+				)
+				lg.draw(self.image, self.quad, tx, ty)
+			end
+		end
+	end
+end
+
+function tile_system:register(kernel, order)
+	ferris.base_system.do_default_register(self, kernel, order)
+end
+
+return tile_system
