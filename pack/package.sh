5d64b4231b935d6f59d27de0579e6dc0a66ce5f4
Max Cahill <1bardesign@gmail.com>
2022-01-22 21:15:08 +1100

    added packaging basics; should change love.exe icon for something ferris wheel themed but no time now

---
+++ pack/package.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -e
+
+#initial setup
+rm -rf dist
+rm -f ferris-examples.love
+rm -f ferris-examples-win.zip
+
+#raw love2d file
+cd ..
+zip -r pack/ferris-examples.love *.lua src assets lib
+cd pack
+
+#windows
+mkdir dist
+cat ./win/love.exe ferris-examples.love > dist/ferris-examples.exe
+cp ./win/*.dll dist
+#copy licensing
+cp ./win/license.txt dist/license_love2d.txt
+cp ../lib/batteries/license.txt dist/license_batteries.txt
+cp ../lib/ferris/license.txt dist/license_ferris.txt
+#copy readme
+cp ../readme.md dist/readme.md
+#zip everything up
+cd dist
+zip -r ../ferris-examples-windows.zip .
+cd ..
+rm -rf dist
